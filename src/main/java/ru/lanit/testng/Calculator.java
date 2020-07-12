3b9986bb7082f713121db5ef0c4bd1e884954a08
Dmitriy <ivanovdm812@gmail.com>
2020-07-12 18:42:43 +0400

    Introduce Calculator class

--- src/main/java/ru/lanit/testng/Calculator.java
+++ src/main/java/ru/lanit/testng/Calculator.java
@@ -1,21 +1,25 @@
 package ru.lanit.testng;
 
 public class Calculator {
+
     public static final double EPS = 1E-7;
 
     public static double addition(double a, double b) {
-        return 0;
+        return a + b;
     }
 
     public static double subtraction(double a, double b) {
-        return 0;
+        return a - b;
     }
 
     public static double multiplication(double a, double b) {
-        return 0;
+        return a * b;
     }
 
     public static double division(double a, double b) {
-        return 0;
+        if (Math.abs(b) <= EPS) {
+            throw new ArithmeticException("division by zero");
+        }
+        return a / b;
     }
 }
